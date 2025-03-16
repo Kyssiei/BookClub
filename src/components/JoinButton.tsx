a0b18f75e07e317a9bf909783cc2e0652b4579eb
kyssiei <kyssiei.kay@gmail.com>
2025-03-16 19:24:34 -0400

    added signup button and linked form

--- src/components/JoinButton.tsx
+++ src/components/JoinButton.tsx
@@ -8,7 +8,17 @@ interface JoinButtonProps{
 const JoinButton: React.FC<JoinButtonProps> = ({ onClick }) => {
 
     return(
-        <button onClick={onClick}>Join Now!</button>
+        <button onClick={onClick}
+        style={{
+            width:"200px", 
+            borderRadius:"50px", 
+            border:"1px solid black", 
+            backgroundColor:"rgb(255, 133, 204)",
+            padding:"15px",
+            fontSize:"1.5em",
+            color:"white"
+        }}
+        >Join Now!</button>
     )
 
 };
--- src/components/JoinForm.tsx
+++ src/components/JoinForm.tsx
@@ -1,29 +1,95 @@
 import React, { useState } from "react"
 
-interface JoinFormProps {
-   onSubmit: (name: string, email: string) => void;
+interface FormData {
+   name: string, 
+   email: string,
+   phone: string,
+   birthday: string,
+   username: string,
+   favbookgenere: string ,
 }
 
 
-const JoinForm: React.FC<JoinFormProps> = ({onSubmit}) => {
-    const [name, setName] = useState<string>("");
-    const [email, setEmail] = useState<string>("");
+const JoinForm: React.FC = () =>{
+    const [formData, setFormData] = useState<FormData>({
+        name:"",
+        email:"",
+        phone:"",
+        birthday:"",
+        username:"",
+        favbookgenere:""
+    });
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        onSubmit(name, email);
-    };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const {name, value} = e.target;
+        setFormData((prevData) => ({...prevData, [name]: value }));
+    }
+
+    
 
     return(
-       <form onSubmit={handleSubmit}>
-            <input 
-            type="text" 
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Name" 
-            required/>
-       </form>
+        <div>
+            <form action="">
+
+                <input 
+                name="name"
+                type="text" 
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleChange}
+                />
+
+                <input 
+                name="email"
+                type="text" 
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                />
+
+                <input 
+                name="phone"
+                type="text" 
+                placeholder="Phone"
+                value={formData.phone}
+                onChange={handleChange}
+                />
+
+                <input 
+                name="birthday"
+                type="text" 
+                placeholder="Birthday"
+                value={formData.birthday}
+                onChange={handleChange}
+                />
+
+                <input 
+                name="username"
+                type="text" 
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+                />
+
+                <select 
+                name="favbookgenere"
+                // placeholder="Favorite Book Genre"
+                value={formData.favbookgenere}
+                onChange={handleChange}
+                >
+                    <option value="">Select Favorite Genere</option>
+                    <option value="fiction"></option>
+                    <option value="non-fiction"></option>
+                    <option value="mystery"></option>
+                    <option value="fantasy"></option>
+                    <option value="romance"></option>
+                    <option value="science-fiction"></option>
+                    <option value="self-help"></option>
+                    <option value="other"></option>
+                </select>
+            </form>
+        </div>
     )
-    
 }
+
 export default JoinForm
